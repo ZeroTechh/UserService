2869a1d2d7878861cb8505b2f76ec938323531d1
Roshan Mehta <sonicroshan122@gmail.com>
2020-04-01 21:18:11 +0530

    Added Auth function in main data handler

--- core/userMain/userMain_test.go
+++ core/userMain/userMain_test.go
@@ -12,28 +12,32 @@ import (
 func TestMain(t *testing.T) {
 	assert := assert.New(t)
 
-	Main := NewMain()
+	main := NewMain()
 
 	// Testing that Main can add and get user Main data
 	data, _ := utils.GetMockUserData()
 	filter := types.Main{UserID: data.UserID}
-	msg := Main.Create(data)
+	msg := main.Create(data)
 	assert.Zero(msg)
-	assert.Equal(data, Main.Get(filter))
+	assert.Equal(data, main.Get(filter))
 
 	// Testing that it returns invalid data message for invalid data
-	msg = Main.Create(types.Main{})
+	msg = main.Create(types.Main{})
 	assert.NotZero(msg)
 
-	// Testing that Main cann update user Main data
+	// Testing that Main can update user Main data
 	data, _ = utils.GetMockUserData() // generating new data to get new username
 	name := data.Username
 	update := types.Main{Username: name}
-	msg = Main.Update(filter, update)
+	msg = main.Update(filter, update)
 	assert.Zero(msg)
-	assert.Equal(name, Main.Get(filter).Username)
+	assert.Equal(name, main.Get(filter).Username)
 
 	// Testing that it returns invalid data message for invalid data
-	msg = Main.Update(filter, types.Main{Username: "CC"}) // should fail as there are capital letters
+	msg = main.Update(filter, types.Main{Username: "CC"}) // should fail as there are capital letters
 	assert.NotZero(msg)
+
+	// Testing Auth
+	assert.True(main.Auth(data.Username, "", data.Password))
+	assert.False(main.Auth("invalidUsername", "", data.Password))
 }
